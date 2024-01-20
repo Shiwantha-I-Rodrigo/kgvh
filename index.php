24b377b590bcc49aca717e717f884bc235e78460
Shiwantha-I-Rodrigo <shiwantha.indunil@gmail.com>
2024-01-20 05:25:39 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <h1>King Garden View Hotel</h1>
+
+</body>
+<!--sudo vim /etc/lighttpd/lighttpd.conf-->
+<!--sudo vim /etc/php/php.ini-->
+<!--sudo vim /var/log/lighttpd/error.log-->
+<!--sudo cp diffie_hellman.php ../lighttpd_root/index.php-->
+
+</html>
