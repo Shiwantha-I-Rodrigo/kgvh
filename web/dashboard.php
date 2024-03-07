4102e1cff17d58e363a6d49257670cb50885aa02
Shiwantha-I-Rodrigo <shiwantha.indunil@gmail.com>
2024-03-07 05:56:02 +0100

    customer_login

--- web/dashboard.php
+++ web/dashboard.php
@@ -1,33 +1,33 @@
 <?php
 session_start();
-if(!isset($_SESSION['user_id'])){
+if (!isset($_SESSION['user_id'])) {
     header("Location:login.php");
 }
 include 'header.php';
 ?>
 
-<main id="main">
-    <section class="breadcrumbs">
-        <div class="container">
 
-            <div class="d-flex justify-content-between align-items-center">
-                <h2>Dashboard</h2>
-                <ol>
-                    <li><a href="index.html"><?php $_SESSION['user_id']?></a></li>
-                    <li>Dashboard</li>
-                </ol>
+<main id="main">
+    <section id="contact" class="contact">
+        <div class="container ">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="titlepage">
+                        <h2>Dashboard</h2>
+                    </div>
+                </div>
             </div>
 
-        </div>
-    </section>
-    <section class="inner-page">
-        <div class="container">
-            <p>
-                Dashboard Area
-            </p>
+            <div class="row justify-content-center">
+                <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+                    <h3><?php echo $_SESSION['user_id']; ?></h3>
+                </div>
+            </div>
         </div>
     </section>
 </main>
+
 
 <?php
 include 'footer.php';
