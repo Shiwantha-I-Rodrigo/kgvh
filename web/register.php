57f35b75bc5591d680322eb4770d2485d349bd42
Shiwantha-I-Rodrigo <shiwantha.indunil@gmail.com>
2024-03-10 22:28:03 +0100

    registration_submit_confirm_modal

--- web/register.php
+++ web/register.php
@@ -28,9 +28,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (empty($last_name)) {
         $message['last_name'] = "The last name should not be blank...!";
     }
-    if (!isset($gender)) {
-        $message['gender'] = "Gender is required";
-    }
 
     //Advance validation and submit-------------------------------------
     if (ctype_alpha(str_replace(' ', '', $first_name)) === false) {
@@ -73,8 +70,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $user_id = $db->insert_id;
         $reg_no = date('Y') . date('m') . $user_id;
         $_SESSION['reg_no'] = $reg_no;
+        $_SESSION['user_name'] = $user_name;
 
-        $sql = "INSERT INTO `customers`(`FirstName`, `LastName`, `Email`, `AddressLine1`, `AddressLine2`, `AddressLine3`, `TelNo`, `MobileNo`, `Gender`, `DistrictId`,`RegNo`,`UserId`) VALUES ('$first_name','$last_name','$email','$address_line1','$address_line2','$address_line3','$telno','$mobile_no','$gender','$district','$reg_no','$user_id')";
+        $sql = "INSERT INTO `customers`(`FirstName`, `LastName`, `Email`, `AddressLine1`, `AddressLine2`, `AddressLine3`, `TelNo`, `MobileNo`, `Gender`, `DistrictId`,`RegNo`,`UserId`) VALUES ('$first_name','$last_name','$email','$address_line1','$address_line2','$address_line3','$telephone','$mobile','$gender','$nationality','$reg_no','$user_id')";
         $db->query($sql);
 
         $msg = "<h1>SUCCESS</h1>";
@@ -97,7 +95,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <div class="row g-0">
                             <div class="col-md-6 col-lg-12 d-flex align-items-center">
                                 <div class="card-body p-4 p-lg-5 text-black">
-                                    <form class="main_form" action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" role="form" novalidate>
+                                    <form class="main_form" id="reg_form" action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" role="form" novalidate>
+
                                         <div class="d-flex align-items-center mb-3 pb-1">
                                             <img src="images/logo_big.png" alt="login form" class="img-fluid full" style="border-radius: 1rem 0 0 1rem;" />
                                         </div>
@@ -106,33 +105,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                                         <label class="halfL">Username</label>
                                         <label class="halfR">Password</label>
+                                        <span class="text-danger halfL"><?= @$message['user_name'] ?></span>
+                                        <span class="text-danger halfR"><?= @$message['password'] ?></span>
                                         <input type="text" class="form-control inputs halfL" name="user_name" id="user_name" placeholder="Username *" required />
                                         <input type="password" class="form-control inputs halfR" name="password" id="password" placeholder="Password *" required />
-                                        <span class="text-danger"><?= @$message['credentials'] ?></span>
+                                        
 
                                         <label class="halfL">Email</label>
+                                        <span class="text-danger halfR"><?= @$message['email'] ?></span>
                                         <input type="email" class="form-control inputs" name="email" id="email" placeholder="Email *" required />
-                                        <span class="text-danger"><?= @$message['email'] ?></span>
 
                                         <label class="halfL">First Name</label>
                                         <label class="halfR">Last Name</label>
-                                        <input type="text" class="form-control inputs halfL" name="first_name" id="first_name" placeholder="First Name *" required />
-                                        <input type="text" class="form-control inputs halfR" name="last_name" id="last_name" placeholder="Last Name *" required />
                                         <span class="text-danger halfL"><?= @$message['first_name'] ?></span>
                                         <span class="text-danger halfR"><?= @$message['last_name'] ?></span>
+                                        <input type="text" class="form-control inputs halfL" name="first_name" id="first_name" placeholder="First Name *" required />
+                                        <input type="text" class="form-control inputs halfR" name="last_name" id="last_name" placeholder="Last Name *" required />
 
                                         <label class="halfL">NIC</label>
                                         <label class="halfR">Birthday</label>
-                                        <input type="text" class="form-control inputs halfL" name="nic" id="nic" placeholder="National ID Card Number" />
-                                        <input type="date" class="form-control inputs halfR" name="birthday" id="birthday" placeholder="Birth Day" />
                                         <span class="text-danger halfL"><?= @$message['nic'] ?></span>
                                         <span class="text-danger halfR"><?= @$message['birthday'] ?></span>
+                                        <input type="text" class="form-control inputs halfL" name="nic" id="nic" placeholder="National ID Card Number" />
+                                        <input type="date" class="form-control inputs halfR" name="birthday" id="birthday" placeholder="Birth Day" />
 
                                         <label class="halfL">Address</label>
+                                        <span class="text-danger halfR"><?= @$message['address'] ?></span>
                                         <input type="text" class="form-control inputs" name="address1" id="address1" placeholder="House/Building No." />
                                         <input type="text" class="form-control inputs" name="address2" id="address2" placeholder="Street" />
                                         <input type="text" class="form-control inputs" name="address3" id="address3" placeholder="Town" />
-                                        <span class="text-danger"><?= @$message['address'] ?></span>
 
                                         <?php
                                         $db = dbConn();
@@ -160,22 +161,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                                         <label class="halfL">Mobile</label>
                                         <label class="halfR">Telephone</label>
-                                        <input type="text" class="form-control inputs halfL" name="mobile" id="mobile" placeholder="Mobile No." />
-                                        <input type="text" class="form-control inputs halfR" name="telephone" id="telephone" placeholder="Telephone No." />
                                         <span class="text-danger halfL"><?= @$message['mobile'] ?></span>
                                         <span class="text-danger halfR"><?= @$message['telephone'] ?></span>
+                                        <input type="text" class="form-control inputs halfL" name="mobile" id="mobile" placeholder="Mobile No." />
+                                        <input type="text" class="form-control inputs halfR" name="telephone" id="telephone" placeholder="Telephone No." />
 
                                         <label class="halfL">About You</label>
+                                        <span class="text-danger halfR"><?= @$message['about'] ?></span>
                                         <input type="text" class="form-control inputs" name="about" id="about" placeholder="Something About You " />
-                                        <span class="text-danger"><?= @$message['about'] ?></span>
-
-                                        <button class="common_btn full" type="submit" formmethod="post">Submit</button>
-
-                                        <p style="color: #fff;"> Already have an account ? <a href="login.php" style="color: #a5c5c5;"> Login here </a></p>
-                                        <p style="color: #fff; margin-top: 20px;" class="text-muted"> ! Required fields are indicated with a '*' mark </p>
-                                        <a href="index.php" class="small text-muted">Terms of use.</a>
-                                        <a href="index.php" class="small text-muted">Privacy policy</a>
+                                        
                                     </form>
+                                    <button class="common_btn full" data-toggle="modal" data-target="#success_modal">Submit</button>
+                                    <p style="color: #fff;"> Already have an account ? <a href="login.php" style="color: #a5c5c5;"> Login here </a></p>
+                                    <p style="color: #fff; margin-top: 20px;" class="text-muted"> Required fields are indicated with a '*' mark </p>
+                                    <a href="index.php" class="small text-muted">Terms of use.</a>
+                                    <a href="index.php" class="small text-muted">Privacy policy</a>
                                 </div>
                             </div>
                         </div>
@@ -186,6 +186,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </section>
 </div>
 
+
+<!-- Modal -->
+<div class="modal fade" id="success_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Submition Confirmation</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Are You Sure You Want to Submit the Registration ?</p>
+            </div>
+            <div class="modal-footer">
+                <button class="crit_btn halfL" data-dismiss="modal" aria-label="Close">Cancel</button>
+                <button class="common_btn halfR" type="submit" form="reg_form" formmethod="post">Confirm</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php
